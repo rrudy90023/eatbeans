15a88fa1d4fcacdccd971f302bff879f412e4e2c
Rudy Perez <rrudy90023@gmail.com>
2014-01-05 23:20:12 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eatbeans::Application.config.secret_key_base = '9357afc94398fa2eb4a1a1dea5255622e33cbb407f9d7ff5127d9ed9df15de6de041eac809f8bdbe9635ec89624ed19fddcb802d6f29e3e64b891696650016e6'
